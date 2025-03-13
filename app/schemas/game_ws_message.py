a95f28f7e6ddf02d54f35f081edee759d67ad0e3
Hot6Mania <than7683@gmail.com>
2025-03-14 02:18:18 +0900

    change variable name id to uid

--- app/schemas/game_ws_message.py
+++ app/schemas/game_ws_message.py
@@ -1,13 +1,11 @@
 from typing import Any, Literal
 
 from pydantic import BaseModel
-from services.score_calculator.enums.enums import Wind, Yaku
 
+# 밑은 예시이고 변경가능
 ActionType = Literal[
     "authenticateRequest",
     "authenticateResponse",
-    "scoreCheckRequest",
-    "scoreCheckResponse",
 ]
 
 
@@ -19,57 +17,25 @@ class BaseGameWebSocketMessage(BaseModel):
         extra = "forbid"
 
 
-class AuthenticateData(BaseModel):
+class AuthenticateRequestData(BaseModel):
     access_token: str
-    client_id: str
+    client_uid: str
 
 
 class AuthenticateResponseData(BaseModel):
     success: bool
     message: str
-    client_id: str | None
+    client_uid: str | None
 
 
-class ScoreCheckData(BaseModel):
-    hand: str
-    winning_tile: str
-    is_discarded: bool
-    seat_wind: Wind
-    round_wind: Wind
-    is_last_tile_in_the_game: bool
-    is_last_tile_of_its_kind: bool
-    is_replacement_tile: bool
-    is_robbing_the_kong: bool
-
-
-class ScoreCheckResponseData(BaseModel):
-    total_score: int
-    yaku_score_list: list[tuple[Yaku, int]]
-
-
-class AuthenticateRequestMessage(BaseGameWebSocketMessage):
+class AuthenticateRequest(BaseGameWebSocketMessage):
     action: Literal["authenticateRequest"]
-    data: AuthenticateData
+    data: AuthenticateRequestData
 
 
-class AuthenticateResponseMessage(BaseGameWebSocketMessage):
+class AuthenticateResponse(BaseGameWebSocketMessage):
     action: Literal["authenticateResponse"]
     data: AuthenticateResponseData
 
 
-class ScoreCheckRequestMessage(BaseGameWebSocketMessage):
-    action: Literal["scoreCheckRequest"]
-    data: ScoreCheckData
-
-
-class ScoreCheckResponseMessage(BaseGameWebSocketMessage):
-    action: Literal["scoreCheckRequest"]
-    data: ScoreCheckResponseData
-
-
-GameWebSocketMessage = (
-    AuthenticateRequestMessage
-    | AuthenticateResponseMessage
-    | ScoreCheckRequestMessage
-    | ScoreCheckResponseMessage
-)
+GameWebSocketMessage = AuthenticateRequest | AuthenticateResponse
