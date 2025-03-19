4d34d270e1a60434e820b3e558567431bec7ab05
Hot6Mania <than7683@gmail.com>
2025-03-19 12:13:00 +0900

    add router

--- app/api/v1/endpoints/__init__.py
+++ app/api/v1/endpoints/__init__.py
@@ -4,4 +4,4 @@ from app.api.v1.endpoints import game_ws, score_check
 
 api_router = APIRouter()
 api_router.include_router(score_check.router, tags=["score-check"])
-api_router.include_router(game_ws.router, tags="game-ws")
+api_router.include_router(game_ws.router, tags=["game-ws"])
