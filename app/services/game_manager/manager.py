ec80a48b83f9f594fbf4dfa02097edcdf08cdf0f
Hot6Mania <than7683@gmail.com>
2025-06-11 14:44:30 +0900

    fix gametile bug

--- app/services/game_manager/manager.py
+++ app/services/game_manager/manager.py
@@ -85,7 +85,7 @@ class RoundManager:
         player_list = self.game_manager.player_list
 
         hands = [
-            [t.value for t in self.hands[i].tiles.elements()]
+            [tt.value for (tt := GameTile(t)) in self.hands[i].tiles.elements()]
             for i in range(self.game_manager.MAX_PLAYERS)
         ]
 
